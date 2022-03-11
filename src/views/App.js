51b3bd642ca817fad33da0c22888f2d1b6468378
youngngreen <lenamtienthanh@gmail.com>
2022-03-12 03:18:03 +0700

    todo app

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.scss';
-import MyComponent from './Example/MyComponent'
+// import MyComponent from './Example/MyComponent'
+import ListTodo from './Todos/ListTodo';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -8,12 +11,25 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          xin chao cac ban
+          To Do App With Thanh Le
         </p>
 
-        <MyComponent/>
+        {/* <MyComponent/> */}
+
+        <ListTodo />
 
       </header>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
---
+++ src/views/Todos/ListTodo.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './ListTodo.scss';
+import AddTodo from './AddTodo';
+import { toast } from 'react-toastify';
+
+class ListTodo extends React.Component {
+
+    state = {
+        listTodos: [
+            {id: 'todo1', title: 'Doing homework'},
+            {id: 'todo2', title: 'Making videos'},
+            {id: 'todo3', title: 'Fixing bugs'}
+        ]
+    }
+
+    addNewTodo = (todo) => {
+        this.setState({
+            listTodos: [...this.state.listTodos, todo]
+        })
+
+        toast.success("Wow so easy!");
+
+    }
+
+    render () {
+        let {listTodos} = this.state;
+        return (
+            <div className="list-todo-container">
+                <AddTodo
+                    addNewTodo = {this.addNewTodo}
+                />
+                <div className="list-todo-content">
+                    {listTodos && listTodos.length > 0 &&
+                        listTodos.map((item, index) => {
+                            return (
+                                <div className="todo-child" key={item.id}>
+                                    <span>{index + 1} - {item.title}</span>
+                                    <button className="edit">Edit</button>
+                                    <button className="delete">Delete</button>
+                                </div>
+                            )
+                        })    
+                    }
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ListTodo
